19536a18590e7e0ee1bd3e6f9bd132c19a921439
Ahmed-Raza4 <hafizahmedraza12345@gmail.com>
2024-09-29 19:19:32 +0500

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function footer(){
+    return(
+        <footer>
+            <p className="h-[12vh] border-2 border-solid border-red-800 text-[44px] text-center font-medium bg-yellow-800 text-white font-mono">This is the footer [Using import module]</p>
+        </footer>
+    )
+}
+
+
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function header(){
+    return(
+        <div className="border-2 border-dashed border-red-800 text-[44px] text-center font-bold bg-amber-600 text-white">Header Component [Using import module]</div>
+    )
+}
+
+
